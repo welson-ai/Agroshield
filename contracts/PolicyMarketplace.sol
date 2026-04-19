9ec1adacca06445778a9dfcfad8bd1fb6d1f159b
h <metanexus8@gmail.com>
2026-04-19 16:58:10 +0300

    Fix #49: Update OpenZeppelin ReentrancyGuard import paths
    - Fix ReentrancyGuard import from security/ to utils/ directory
    - Update imports in WeatherPrediction, DynamicPremiums, InsurancePoolStaking, MultiCropPolicy, and PolicyMarketplace contracts
    - Resolve HHE902 error preventing script execution
    - Ensure compatibility with latest OpenZeppelin v5.0.0

--- contracts/PolicyMarketplace.sol
+++ contracts/PolicyMarketplace.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./AgroShieldPolicy.sol";
 import "./AgroShieldOracle.sol";
--- contracts/WeatherPrediction.sol
+++ contracts/WeatherPrediction.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./AgroShieldOracle.sol";
 import "./DynamicPremiums.sol";
 
