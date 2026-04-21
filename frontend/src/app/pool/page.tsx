03be081acd484b0d73f38e3df434f71f6595a001
h <metanexus8@gmail.com>
2026-04-21 23:24:29 +0300

    Update #262: Increase padding on PoolPage from p-4 to p-6

--- frontend/src/app/pool/page.tsx
+++ frontend/src/app/pool/page.tsx
@@ -3,7 +3,7 @@ import { LiquidityPool } from '@/components/liquidity-pool'
 // Pool page component for liquidity management
 export default function PoolPage() {
   return (
-    <div className="p-4">
+    <div className="p-6">
       <LiquidityPool />
     </div>
   )
