4a63f782378069b81bd6fb780ac64ff377e8e356
h <metanexus8@gmail.com>
2026-04-23 13:31:44 +0300

    Update #425: Fix input disabled state and accessibility

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({ className, type, disabled, ...props }: React.ComponentProps<"input">) {
   return (
     <input
       type={type}
@@ -16,6 +16,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       aria-describedby={props['aria-describedby']}
       aria-labelledby={props['aria-labelledby']}
       autoComplete={props.autoComplete}
+      disabled={disabled}
       {...props}
     />
   )
